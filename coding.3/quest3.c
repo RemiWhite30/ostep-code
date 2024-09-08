92041cf6ad91be555f593ad34e0de32bca7371ee
RemiWhite30 <143155255+RemiWhite30@users.noreply.github.com>
2024-09-08 22:50:23 +0000

    Created quest3 source file.

---
+++ coding.3/quest3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]){
+  int rc = fork();
+  if (rc < 0) {
+    fprintf(stderr, "fork failed\n");
+    exit(1);
+  } else if (rc == 0) {
+    printf("hello\n");
+  } else {
+    printf("goodbye\n");
+  }
+  return 0;
+}
+/*
+Remi White
+I am not aware of a way to ensure the child process runs first without calling wait.
+*/
