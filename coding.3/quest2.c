05e6c4882b86d5b41391736e61715e3168eca1e0
RemiWhite30 <143155255+RemiWhite30@users.noreply.github.com>
2024-09-09 02:46:32 +0000

    Created quest2 source file.

---
+++ coding.3/quest2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[]) {
+  int fd = open("./tempfile.txt", O_WRONLY|O_CREAT|O_TRUNC, S_IRWXU);
+  int rc = fork();
+  if (rc < 0) {
+    fprintf(stderr, "fork failed\n");
+    exit(1);
+  } else if (rc == 0) {
+    printf("Child\n");
+    write(fd, "I am the child\n", 16);
+  } else {
+    printf("Parent\n");
+    write(fd, "I am the parent\n", 17);
+  }
+  return 0;
+}
+/*
+Remi White
+
+*/
