ffbdebc5a1d6ee4fa8d19ca0999e7f0fe5fe10bc
RemiWhite30 <143155255+RemiWhite30@users.noreply.github.com>
2024-09-09 02:46:55 +0000

    Created quest7 source file.

---
+++ coding.3/quest7.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  int rc = fork();
+  if (rc < 0) {
+    fprintf(stderr, "fork failed\n");
+    exit(1);
+  } else if (rc == 0) {
+    close(STDOUT_FILENO);
+    printf("Child");
+  } else {
+    printf("Parent\n");
+  }
+  return 0;
+}
+/*
+Remi White
+*/
